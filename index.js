c1fa1b7b61bba86b67b35d3e53d9debbf1b94d7f
kayondoterry <kayondoterry@gmail.com>
2021-12-20 19:37:20 +0300

    added easypay callback url

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("<h1>MM Card Application</h1>");
 });
 
+app.get("/easypay", (req, res) => {
+  res.send("Easy Pay Call back url");
+});
+
 server.listen(8000, ()=> {
   console.log("Server running on port 8000");
 });
